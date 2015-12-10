7caaddf0c11b53785bdcd8587e611d4e7cfa03c1
ballislife24 <210766@hkis.edu.hk>
2015-12-10 08:31:08 +0800

    changed images

--- gamedev.js
+++ gamedev.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 
 	$("#goRight1").click(function() {
-		$("#exploreBody").css("background-image", "url('http://www.download3dhouse.com/wp-content/uploads/2013/08/Chinese-hotel-lifts-and-stairs-area.jpg')");
+		$("#exploreBody").css("background-image", "url('http://www.hotelfagus.org/wp-content/uploads/2015/06/feat.jpg')");
 		$("#goRight1").hide();
 		$("#goBack").hide();
 		$("#goRight-Straight1").show();
@@ -9,38 +9,38 @@ $( document ).ready(function() {
 	});
 
 	$("#goRight-Straight1").click(function() {
-		$("#exploreBody").css("background-image", "url('')");
+		$("#exploreBody").css("background-image", "url('file:///Users/s210766/Desktop/gameTemplate/img/Chinese-hotel-lifts-and-stairs-area.jpg')");
 		$("#goRight-Straight1").hide();
 		$("#goRight-Straight2").show();
 	});
 
 	$("#goRight-Straight2").click(function() {
-		$("#exploreBody").css("background-image", "url('img/exploreRight3.jpg')");
+		$("#exploreBody").css("background-image", "url('http://megawattmedia.com.au/blog/wp-content/uploads/2010/03/shafted.jpg')");
 		$("#goRight-Straight2").hide();
 		$("#goRight-Straight3").show();
 	});
 
 	$("#goRight-Straight3").click(function() {
-		$("#exploreBody").css("background-image", "url('img/exploreRight3.jpg')");
+		$("#exploreBody").css("background-image", "url('http://www.portaline.gr/en/images/Hotel_Doors/lrg/Hotel_Doors04.jpg')");
 		$("#goRight-Straight3").hide();
 		$("#goRight-Right2").show();
 	});
 
 	$("#goRight-Right2").click(function() {
-		$("#exploreBody").css("background-image", "url('img/explore2-goBack2.jpg')");
+		$("#exploreBody").css("background-image", "url('https://palacestation.sclv.com/~/media/Images/Page%20Background%20Images/Palace/Hotel/PS_Hotel_KingRoom_new.jpg')");
 		$("#goRight-Right2").hide();
 		$("#goBack").show();
 	});
 
 
 	$("#goLeft1").click(function() {
-		$("#exploreBody").css("background-image", "url('img/exploreLeft1.jpg')");
+		$("#exploreBody").css("background-image", "url('ihttps://s-media-cache-ak0.pinimg.com/736x/ce/fd/a2/cefda2fd973aed3235281501929a78db.jpg')");
 		$("#goLeft1").hide();
 		$("#goLeft2").show();
 	});
 
 	$("#goLeft-Straight1").click(function() {
-		$("#exploreBody").css("background-image", "url('img/exploreLeft2.jpg')");
+		$("#exploreBody").css("background-image", "url('http://cloudimages.youthconnect.in/wp-content/uploads/2013/11/Sachin-Tendulkar.jpg')");
 		$("#goLeft2").hide();
 		$("#goLeft3").show();
 	});
